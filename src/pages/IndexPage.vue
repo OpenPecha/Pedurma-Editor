4a0ffe0c166da39ac188bf9969089392d742fd36
test <ten13zin@gmail.com>
2024-03-22 12:38:35 +0530

    add editors card

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,37 @@
 <template>
-  <q-page class="flex flex-center"> </q-page>
+  <q-page class="flex flex-center">
+    <div class="row q-gutter-lg">
+      <q-card
+        v-ripple
+        flat
+        bordered
+        style="width: 300px"
+        class="column cursor-pointer q-hoverable"
+        @click="open('/text')"
+      >
+        <q-img src="~/assets/img/pedurma_text.png" style="height: 200px" />
+        <q-separator />
+        <q-card-section class="bg-primary text-white">
+          <div class="text-h5 q-mt-sm q-mb-xs">Text Editor</div>
+        </q-card-section>
+      </q-card>
+
+      <q-card
+        v-ripple
+        flat
+        bordered
+        style="width: 300px"
+        class="column cursor-pointer q-hoverable"
+        @click="open('/note')"
+      >
+        <q-img src="~/assets/img/pedurma_note.png" style="height: 200px" />
+        <q-separator />
+        <q-card-section class="bg-primary text-white">
+          <div class="text-h5 q-mt-sm q-mb-xs">བསྡུར་མཆན་ Editor</div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
 </template>
 
 <script>
@@ -7,5 +39,15 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "IndexPage",
+
+  methods: {
+    open(path) {
+      if (path.match("https")) {
+        openURL(path);
+      } else {
+        this.$router.push(path);
+      }
+    },
+  },
 });
 </script>
